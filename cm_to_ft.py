db4db8ce451a776f299694df97da9de5d94e68f5
BimboScape <mackenzie.chevalier2003@gmail.com>
2024-08-05 21:22:28 +0000

    Add files via upload

---
+++ cm_to_ft.py
@@ -0,0 +1,13 @@
+#cm x 0.0328084 = ft
+welcome = ("welcome to kenz's cm to ft converter!")
+close = ("thank you for using kenz's cm to ft converter!")
+
+print(welcome)
+
+num1 = float(input("please input your first number in cm : "))
+num2 = float(0.0328084)
+
+sum = num1 * num2
+
+print(f"{num1} in ft is {sum}ft!")
+print(close)
