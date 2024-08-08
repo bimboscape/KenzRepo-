81cb22a94cc3b4d2e8fb6f55b93ef0b1c8b88417
BimboScape <mackenzie.chevalier2003@gmail.com>
2024-08-08 18:42:35 +0000

    Update Directory_maker.py

--- Directory_maker.py
+++ Directory_maker.py
@@ -5,7 +5,12 @@ end = ("enjoy your new directory!")
 
 print(welcome)
 
-os.chdir(input('where shall we be making your new directory?'))
+user_in = input('where shall we be making your new directory?')
+
+os.chdir(user_in)
+
+exists = os.path.exists(user_in)
+print(f"Directory exists : {exists}")
 
 os.mkdir(input('what would you like your new directory to be called? : '))
 
