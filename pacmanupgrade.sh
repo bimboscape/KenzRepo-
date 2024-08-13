7f92ed791e01762f3cd702b62dcd58e6b496d2e0
BimboScape <mackenzie.chevalier2003@gmail.com>
2024-08-13 18:09:35 +0000

    Create pacmanupgrade.sh

---
+++ pacmanupgrade.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sep="-----------------------------"
+wel="Thank you for using Kenzie's upgrade script"
+exit="Please enjoy your newly upgraded OS!"
+echo $wel
+echo $sep
+sudo pacman -Syu
+echo $sep
+sudo pacman -Sc
+sudo $sep
+sudo pacman -Qdtq | pacman -Rns -
+echo $sep
+echo "The script may not have detected all packages."
+echo "Here is a list of packages that may still be useless"
+pacman -Qqd | pacman -Rsu --print -
+echo $exit
